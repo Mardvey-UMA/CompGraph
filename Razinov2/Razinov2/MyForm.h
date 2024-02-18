3306e5540b726a22fcd7239c5dbd921ae5b04de7
Mattew Razinov <chikernut213@gmail.com>
2024-02-19 01:09:21 +0400

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	deleted:    Razinov2/.vs/Razinov2/FileContentIndex/068a7dbc-98b6-4f7a-ad9c-b7ee86826472.vsidx
    	deleted:    Razinov2/.vs/Razinov2/FileContentIndex/56279d7b-ee0c-41e5-b3db-6a4166fd6849.vsidx
    	deleted:    Razinov2/.vs/Razinov2/FileContentIndex/5b2627e2-e255-4a72-9971-f4f7a12606f1.vsidx
    	deleted:    Razinov2/.vs/Razinov2/FileContentIndex/733ae699-23a0-4c0f-93a9-f94acb88cf81.vsidx
    	new file:   Razinov2/.vs/Razinov2/FileContentIndex/7b9dc7cd-f250-427c-8eb6-61df2ddd5efe.vsidx
    	deleted:    Razinov2/.vs/Razinov2/FileContentIndex/84def808-433f-43ad-9cdf-a471c4362a09.vsidx
    	deleted:    Razinov2/.vs/Razinov2/FileContentIndex/8ed37db6-651d-4576-b893-081c9eef105d.vsidx
    	deleted:    Razinov2/.vs/Razinov2/FileContentIndex/9eb27fd4-135e-4b8a-8b58-1568b0e332bf.vsidx
    	deleted:    Razinov2/.vs/Razinov2/FileContentIndex/cb4ad4e5-f7a0-47c3-ac22-85f2785b5a92.vsidx
    	modified:   Razinov2/.vs/Razinov2/v17/.suo
    	modified:   Razinov2/.vs/Razinov2/v17/Browse.VC.db
    	new file:   Razinov2/.vs/Razinov2/v17/ipch/AutoPCH/35ad37b830171419/MYFORM.ipch
    	new file:   Razinov2/.vs/Razinov2/v17/ipch/AutoPCH/3ad5cc526bc86c18/MYFORM.ipch
    	new file:   Razinov2/.vscode/c_cpp_properties.json
    	new file:   Razinov2/.vscode/settings.json
    	modified:   Razinov2/Razinov2/MyForm.h
    	modified:   Razinov2/Razinov2/x64/Debug/.NETFramework,Version=v4.7.2.AssemblyAttributes.obj
    	modified:   Razinov2/Razinov2/x64/Debug/MyForm.obj
    	modified:   Razinov2/Razinov2/x64/Debug/Razinov2.exe.recipe
    	modified:   Razinov2/Razinov2/x64/Debug/Razinov2.log
    	modified:   Razinov2/Razinov2/x64/Debug/Razinov2.tlog/CL.command.1.tlog
    	modified:   Razinov2/Razinov2/x64/Debug/Razinov2.tlog/CL.read.1.tlog
    	modified:   Razinov2/Razinov2/x64/Debug/Razinov2.tlog/CL.write.1.tlog
    	modified:   Razinov2/Razinov2/x64/Debug/Razinov2.tlog/Razinov2.lastbuildstate
    	modified:   Razinov2/Razinov2/x64/Debug/Razinov2.tlog/ResGen.command.1.tlog
    	modified:   Razinov2/Razinov2/x64/Debug/Razinov2.tlog/ResGen.read.1.tlog
    	modified:   Razinov2/Razinov2/x64/Debug/Razinov2.tlog/ResGen.write.1.tlog
    	modified:   Razinov2/Razinov2/x64/Debug/Razinov2.tlog/link.command.1.tlog
    	modified:   Razinov2/Razinov2/x64/Debug/Razinov2.tlog/link.read.1.tlog
    	modified:   Razinov2/Razinov2/x64/Debug/Razinov2.tlog/link.write.1.tlog
    	modified:   Razinov2/Razinov2/x64/Debug/Razinov2.vcxproj.FileListAbsolute.txt
    	modified:   Razinov2/Razinov2/x64/Debug/vc143.pdb
    	new file:   Razinov2/coords.txt
    	modified:   Razinov2/x64/Debug/Razinov2.exe
    	modified:   Razinov2/x64/Debug/Razinov2.pdb

--- Razinov2/Razinov2/MyForm.h
+++ Razinov2/Razinov2/MyForm.h
@@ -10,86 +10,83 @@ namespace Razinov2 {
 	using namespace System::Data;
 	using namespace System::Drawing;
 	float lines[] = { // стороны клоуняры
-				8.f,1.f,10.f,1.f,
-				8.f,1.f,7.f,2.f,
-				7.f,2.f,6.f,4.f,
-				6.f,4.f,7.f,6.f,
-				7.f,6.f,5.f,7.f,
-				5.f,7.f,5.f,8.f,
-				5.f,8.f,6.f,9.f,
-				6.f,9.f,8.f,8.f,
-				8.f,8.f,10.f,8.f,
-				5.f,8.f,1.f,13.f,
-				1.f,13.f,1.f,15.f,
-				1.f,15.f,2.f,14.f,
-				2.f,14.f,1.f,16.f,
-				1.f,16.f,2.f,17.f,
-				2.f,17.f,3.f,15.f,
-				3.f,15.f,2.f,18.f,
-				2.f,18.f,4.f,20.f,
-				4.f,20.f,4.f,15.f,
-				4.f,15.f,5.f,13.f,
-				5.f,13.f,5.f,11.f,
-				5.f,11.f,7.f,9.f,
-				7.f,9.f,9.f,9.f,
-				9.f,9.f,10.f,11.5f,
-				10.f,11.5f,9.f,14.f,
-				9.f,14.f,7.f,14.f,
-				7.f,14.f,5.f,13.f,
-				7.f,11.f,8.f,11.f,
-				8.f,11.f,9.f,13.f,
-				9.f,13.f,8.f,14.f,
-				8.f,14.f,8.f,13.f,
-				8.f,13.f,7.f,13.f,
-				7.f,13.f,7.f,14.f,
-				7.f,14.f,6.f,13.f,
-				6.f,13.f,7.f,11.f,
-				9.f,14.f,7.f,15.f,
-				7.f,15.f,6.f,17.f,
-				6.f,17.f,5.f,17.f,
-				5.f,17.f,4.f,18.f,
-				6.f,17.f,7.f,18.f,
-				7.f,18.f,10.f,19.f,
-				6.f,18.f,5.f,19.f,
-				6.f,19.f,7.f,20.f,
-				7.f,20.f,10.f,21.f,
-				10.f,22.f,7.f,21.f,
-				7.f,21.f,5.f,19.f,
-				10.f,24.f,9.f,24.f,
-				9.f,24.f,5.f,22.f,
-				5.f,22.f,4.f,20.f,
-				10.f,14.f,9.f,14.f,
-				10.f,25.f,6.f,29.f,
-				6.f,29.f,6.f,23.f,
-				6.f,23.f,10.f,27.f
+				8.f,29.f,10.f,29.f,
+				8.f,29.f,7.f,28.f,
+				7.f,28.f,6.f,26.f,
+				6.f,26.f,7.f,24.f,
+				7.f,24.f,5.f,23.f,
+				5.f,23.f,5.f,22.f,
+				5.f,22.f,6.f,21.f,
+				6.f,21.f,8.f,22.f,
+				8.f,22.f,10.f,22.f,
+				5.f,22.f,1.f,17.f,
+				1.f,17.f,1.f,15.f,
+				1.f,15.f,2.f,16.f,
+				2.f,16.f,1.f,14.f,
+				1.f,14.f,2.f,13.f,
+				2.f,13.f,3.f,15.f,
+				3.f,15.f,2.f,12.f,
+				2.f,12.f,4.f,10.f,
+				4.f,10.f,4.f,15.f,
+				4.f,15.f,5.f,17.f,
+				5.f,17.f,5.f,19.f,
+				5.f,19.f,7.f,21.f,
+				7.f,21.f,9.f,21.f,
+				9.f,21.f,10.f,18.5f,
+				10.f,18.5f,9.f,16.f,
+				9.f,16.f,7.f,16.f,
+				7.f,16.f,5.f,17.f,
+				7.f,19.f,8.f,19.f,
+				8.f,19.f,9.f,17.f,
+				9.f,17.f,8.f,16.f,
+				8.f,16.f,8.f,17.f,
+				8.f,17.f,7.f,17.f,
+				7.f,17.f,7.f,16.f,
+				7.f,16.f,6.f,17.f,
+				6.f,17.f,7.f,19.f,
+				9.f,16.f,7.f,15.f,
+				7.f,15.f,6.f,13.f,
+				6.f,13.f,5.f,13.f,
+				5.f,13.f,4.f,12.f,
+				6.f,13.f,7.f,12.f,
+				7.f,12.f,10.f,11.f,
+				6.f,12.f,5.f,11.f,
+				6.f,11.f,7.f,10.f,
+				7.f,10.f,10.f,9.f,
+				10.f,8.f,7.f,9.f,
+				7.f,9.f,5.f,11.f,
+				10.f,6.f,9.f,6.f,
+				9.f,6.f,5.f,8.f,
+				5.f,8.f,4.f,10.f,
+				10.f,16.f,9.f,16.f,
+				10.f,5.f,6.f,1.f,
+				6.f,1.f,6.f,7.f,
+				6.f,7.f,10.f,3.f
 	};
 		float uzor[] = {
-			//�����
-			8.f, 26.f, 7.f, 26.f,
-			7.f, 26.f, 7.f, 27.f,
-			7.f, 27.f, 8.f, 27.f,
-			8.f, 27.f, 8.f, 26.f,
-			//�� ���
-			7.f, 28.f, 6.f, 27.f,
-			7.f, 28.f, 6.f, 29.f,
-			//�� ����
-			6.f, 26.f, 7.f, 25.f,
-			7.f, 25.f, 6.f, 24.f
+			8.f,4.f,7.f,4.f,
+			7.f,4.f,7.f,3.f,
+			7.f,3.f,8.f,3.f,
+			8.f,3.f,8.f,4.f,
+			7.f,2.f,6.f,3.f,
+			7.f,2.f,6.f,1.f,
+			6.f,4.f,7.f,5.f,
+			7.f,5.f,6.f,6.f
 		};
 	float star[] = { // звездочка
-				12.f, 7.f, 15.f, 5.f,
-				15.f, 4.f, 16.f, 3.f,
-				16.f, 3.f, 16.f, 4.f,
-				16.f, 4.f, 17.f, 5.f,
-				17.f, 5.f, 16.f, 5.f,
-				16.f, 5.f, 15.f, 6.f,
-				15.f, 6.f, 15.f, 5.f,
-				15.f, 5.f, 14.f, 4.f,
-				14.f, 4.f, 15.f, 4.f,
-				15.f, 4.f, 16.f, 4.f,
-				16.f, 4.f, 16.f, 5.f,
-				16.f, 5.f, 15.f, 5.f,
-				15.f, 5.f, 15.f, 4.f
+				12.f,23.f,15.f,25.f,
+				15.f,26.f,16.f,27.f,
+				16.f,27.f,16.f,26.f,
+				16.f,26.f,17.f,25.f,
+				17.f,25.f,16.f,25.f,
+				16.f,25.f,15.f,24.f,
+				15.f,24.f,15.f,25.f,
+				15.f,25.f,14.f,26.f,
+				14.f,26.f,15.f,26.f,
+				15.f,26.f,16.f,26.f,
+				16.f,26.f,16.f,25.f,
+				16.f,25.f,15.f,25.f,
+				15.f,25.f,15.f,26.f
 	};
 	float mouse[] = { // мышка
 				0.5f,3.f,1.f,4.5f, // �� ����� ���� ����� �� ���
@@ -167,15 +164,9 @@ namespace Razinov2 {
 		MyForm(void)
 		{
 			InitializeComponent();
-			//
-			//TODO: �������� ��� ������������
-			//
 		}
 
 	protected:
-		/// <summary>
-		/// ���������� ��� ������������ �������.
-		/// </summary>
 		~MyForm()
 		{
 			if (components)
@@ -185,16 +176,9 @@ namespace Razinov2 {
 		}
 
 	private:
-		/// <summary>
-		/// ������������ ���������� ������������.
-		/// </summary>
 		System::ComponentModel::Container^ components;
 
 #pragma region Windows Form Designer generated code
-		/// <summary>
-		/// ��������� ����� ��� ��������� ������������ � �� ��������� 
-		/// ���������� ����� ������ � ������� ��������� ����.
-		/// </summary>
 		void InitializeComponent(void)
 		{
 			this->SuspendLayout();
@@ -245,22 +229,22 @@ namespace Razinov2 {
 		if (!printMouse) {
 			// рисуем клоуняру
 			for (int i = 0; i < arrayLength; i += 4) {
-				g->DrawLine(redPen, Sx * lines[i], Sy * lines[i + 1]
-					, Sx * lines[i + 2], Sy * lines[i + 3]);
-				g->DrawLine(redPen, Sx * (10 - lines[i] + 10), Sy * (lines[i + 1])
-					, Sx * (10 - lines[i + 2] + 10), Sy * (lines[i + 3]));
+				g->DrawLine(redPen, Sx * lines[i], Ty - Sy * lines[i + 1]
+					, Sx * lines[i + 2], Ty - Sy * lines[i + 3]);
+				g->DrawLine(redPen, Sx * (10 - lines[i] + 10), Ty - Sy * (lines[i + 1])
+					, Sx * (10 - lines[i + 2] + 10), Ty - Sy * (lines[i + 3]));
 			}
 			
 			for (int i = 0; i < arrayLength2; i += 4) {
-				g->DrawLine(redPen, Sx * uzor[i], Sy * uzor[i + 1]
-					, Sx * uzor[i + 2], Sy * uzor[i + 3]);
-				g->DrawLine(redPen, Sx * (10 - uzor[i] + 10), Sy * (-1 + uzor[i + 1])
-					, Sx * (10 - uzor[i + 2] + 10), Sy * (-1 + uzor[i + 3]));
+				g->DrawLine(redPen, Sx * uzor[i], Ty - Sy * uzor[i + 1]
+					, Sx * uzor[i + 2], Ty - Sy * uzor[i + 3]);
+				g->DrawLine(redPen, Sx * (10 - uzor[i] + 10), Ty - Sy * (1+uzor[i + 1])
+					, Sx * (10 - uzor[i + 2] + 10), Ty - Sy * (1+uzor[i + 3]));
 			}
 
 			for (int i = 0; i < arrayLength3; i += 4) {
-				g->DrawLine(redPen, Sx * star[i], Sy * star[i + 1]
-					, Sx * star[i + 2], Sy * star[i + 3]);
+				g->DrawLine(redPen, Sx * star[i], Ty - Sy * star[i + 1]
+					, Sx * star[i + 2], Ty - Sy * star[i + 3]);
 			}
 		}
 		else {
